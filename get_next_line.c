16d500d0d1d3d19dd4651a78acc0c95976fc4ce8
ttakino <taka3.3chan88@gmail.com>
2024-05-17 18:07:26 +0900

    24.5.17

--- get_next_line.c
+++ get_next_line.c
@@ -14,182 +14,170 @@
 #include <stdio.h>
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 100
 #endif
 
-void	initialize_arr(char *arr, int size)
+char *return_result(char **save)
 {
-	int	i;
+	char *result;
+	char *new_save;
+	int cnt;
+	int i;
 
-	i = 0;
-	while (i < size)
-	{
-		*arr = '\0';
-		arr++;
-		i++;
-	}
-}
+	//printf("\x1b[35m---return_result---\n");
 
-char	*join_save_buf(char *save, char *buf)
-{
-	char	*miniBuf;
-	char	*result;
-	int		i;
-
-	// //printf("\x1b[35m---join_save_buf---\n");
-	if (buf[0] == '\0')
-	{
-		buf = save;
-		save = "";
-	}
-	i = 0;
-	while (buf[i] != '\n' && buf[i] != '\0')
-		i++;
-	if (ft_strchr(save, '\n'))
-		--i;
-	miniBuf = (char *)malloc(i + 2);
-	if (miniBuf == NULL)
+	if (*save == NULL)
 		return (NULL);
-	// //printf("i\t=\t%d\n", i);
-	i = 0;
-	while (buf[i] != '\0' && buf[i] != '\n' && i < BUFFER_SIZE)
-	{		
-		miniBuf[i] = buf[i];
-		i++;
+	cnt = 0;
+	while ((*save)[cnt] != '\n' && (*save)[cnt] != '\0')
+	{
+		cnt++;
 	}
-	if (buf[i] == '\n')
-			miniBuf[i++] = '\n';
-	miniBuf[i] = '\0';
-	// //printf("miniBuf\t=\t%s\n", miniBuf);
-	result = ft_strjoin(save, miniBuf);
-	free(miniBuf);
+
+	// //printf("cnt\t=\t%d\n", cnt);
+
+	if ((*save)[cnt] == '\0')
+	{
+		// result = *save;
+		// free(*save);
+		// *save = "";
+		return (*save);
+	}
+	result = (char *)malloc(cnt + 2);
 	if (result == NULL)
 		return (NULL);
-	// //printf("\x1b[m\n");
+	i = 0;
+	while (i <= cnt)
+	{
+		result[i] = (*save)[i];
+		i++;
+	}
+	result[i] = '\0';
+
+	//printf("result\t=\t%s\n", result);
+
+	while ((*save)[cnt] != '\0')
+		cnt++;
+	new_save = (char *)malloc(cnt - i + 1);
+	if (new_save == NULL)
+	{
+		free(result);
+		return (NULL);
+	}
+
+	// //printf("cnt-i+1\t=\t%d\n", cnt - i + 1);
+
+	cnt = 0;
+	while ((*save)[i + cnt] != '\0')
+	{
+		// //printf("cnt\t=\t%d\n", cnt);
+		// //printf("(*save)[i+cnt]\t=\t%c\n", (*save)[i+cnt]);
+
+		new_save[cnt] = (*save)[i + cnt];
+		cnt++;
+	}
+	// //printf("cnt = %d\n", cnt);
+	new_save[cnt] = '\0';
+
+	// //printf("new_save\t=\t%s\n", new_save);
+
+	if (save[0] != '\0')
+		free(*save);
+	*save = new_save;
+
+	//printf("----------\x1b[m\n");
+
 	return (result);
 }
 
-void	set_next(char *next, char *buf)
+char *return_line(int fd, char *buf)
 {
-	int			i;
-	int			next_i;
-	int			flag;
-
-	//printf("\x1b[31m---set_next---\n");
-	if (ft_strchr(next, '\n') != NULL || buf[0] == '\0')
-	{
-		i = 0;
-		next_i = 0;
-		while (next[i] != '\n')
-			i++;
-		if (next[i] == '\n')
-			i++;
-		while (i + next_i < BUFFER_SIZE && next[i] != '\n')
-		{
-			next[next_i] = next[i + next_i];
-			next_i++;
-		}
-		next[i + next_i] = '\0';
-		//printf("if: i\t=\t%d\nnext_i\t=\t%d\nnext\t=\t%s\n", i, next_i, next);
-		//printf("----------\x1b[m\n");
-		return ;
-	}
-	initialize_arr(next, BUFFER_SIZE);
-	i = 0;
-	next_i = 0;
-	flag = 0;
-	while (i < BUFFER_SIZE && buf[i] != '\0')
-	{
-		if (flag == 1)
-		{
-			next[next_i] = buf[i];
-			next_i++;
-		}
-		if (buf[i] == '\n')
-			flag = 1;
-		i++;
-	}
-	//printf("next\t=\t%s\n", next);
-	//printf("----------\x1b[m\n");
-}
-
-char	*return_line(int fd, char *buf, char *next)
-{
-	static char	*save = "";
-	char		*tmp;
-	char		*result;
-	int			bytes;
+	static char *save = "";
+	char *tmp;
+	char *result;
+	int bytes;
 
 	//printf("\x1b[32m---return_line---\n");
-	// //printf("save\t=\t%s\n", save);
+
+	//printf("save\t=\t%s\n", save);
 	bytes = 0;
 	while (1)
 	{
 		if (ft_strchr(save, '\n') == NULL)
 		{
 			bytes = read(fd, buf, BUFFER_SIZE);
+
+			buf[bytes] = '\0';
 			//printf("buf\t=\t%s\n", buf);
 			//printf("bytes\t=\t%d\n", bytes);
+
+			tmp = save;
+			save = ft_strjoin(tmp, buf);
+			if (save == NULL)
+				return (NULL);
+			if (tmp[0] != '\0')
+			{
+				free(tmp);
+				tmp = NULL;
+			}
 			if (bytes <= 0)
 			{
 				break;
 			}
 		}
-		if (0 < bytes && bytes == BUFFER_SIZE && ft_strchr(buf, '\n') == NULL)
+		if (ft_strchr(save, '\n') != NULL)
 		{
-			save = ft_strjoin(save, buf);
-			if (save == NULL)
-				return (NULL);
-			//printf("if: save\t=\t%s\n", save);
-		}
-		else
-		{
-			//printf("else: save\t=\t%s\n", save);
-			result = join_save_buf(save, buf);
-			//printf("next\t=\t%s\n", next);
-			set_next(next, buf);
-			save = ft_strjoin(next, "");
 			//printf("save\t=\t%s\n", save);
-			if (save == NULL)
+
+			result = return_result(&save);
+			if (result == NULL)
 				return (NULL);
+
+			//printf("\x1b[32m");
+			//printf("save\t=\t%s\n", save);
+			//printf("result\t=\t%s\n", result);
 			//printf("----------\x1b[m\n");
+			//printf("return (result)\n");
+
 			return (result);
 		}
-		initialize_arr(buf, BUFFER_SIZE);
 	}
-	//printf("save = %s\n", save);
+
 	//printf("----------\x1b[m\n");
+
 	if (save[0] != '\0')
 	{
 		tmp = save;
-		save = "";
+		if (save[0] != '\0')
+		{
+			free(save);
+			save = "";
+		}
 		return (tmp);
 	}
 	return (NULL);
 }
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	char		buf[BUFFER_SIZE + 1];
-	static char	next[BUFFER_SIZE] = "";
+	char buf[BUFFER_SIZE + 1];
 
-	initialize_arr(buf, BUFFER_SIZE + 1);
-	// //printf("buf\t=\t%s\n", buf);
-	return (return_line(fd, buf, next));
+	//printf("buf = %s\n", buf);
+	return (return_line(fd, buf));
 }
 
 // #include <fcntl.h>
 
-// int	main(int argc, char **argv)
+// int main(int argc, char **argv)
 // {
-// 	char	*file;
-// 	int		fd;
-// 	char	*line;
-// 	int		cnt;
+// 	char *file;
+// 	int fd;
+// 	char *line;
+// 	int cnt;
 
 // 	if (argc != 2)
 // 	{
-// 		// //printf("指定できるファイルは一個です。\n");
+// 		//printf("指定できるファイルは一個です。\n");
 // 		return (1);
 // 	}
 // 	file = argv[1];
@@ -197,11 +185,11 @@ char	*get_next_line(int fd)
 // 	cnt = 0;
 // 	while (1)
 // 	{
-// 		// //printf("cnt : %d\n", cnt);
+// 		//printf("cnt : %d\n", cnt);
 // 		line = get_next_line(fd);
 // 		if (line == NULL)
 // 		{
-// 			// //printf("\nファイルの読み込みに失敗しました。\n");
+// 			//printf("\nファイルの読み込みに失敗しました。\n");
 // 			return (0);
 // 		}
 // 		//printf("line = %s", line);
---
+++ get_next_line_utils.c
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttakino <ttakino@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 15:34:10 by ttakino           #+#    #+#             */
+/*   Updated: 2024/05/14 16:46:37 by ttakino          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+// size_t	ft_len_to_x(const char *str, char x)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (str[i] != x && str[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*cast_dst;
+	const unsigned char	*cast_src;
+
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	cast_dst = (unsigned char *)dest;
+	cast_src = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		*cast_dst++ = *cast_src++;
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	int				i;
+	unsigned char	cst_c;
+
+	cst_c = (unsigned char)c;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == cst_c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (cst_c == '\0')
+		return ((char *)&str[i]);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+// void	ft_lstadd_back(t_list **lst, t_list *new)
+// {
+// 	t_list	*last;
+
+// 	if (lst == NULL)
+// 	{
+// 		*lst = new;
+// 		return ;
+// 	}
+// 	last = *lst;
+// 	while ((*lst)->next != NULL)
+// 	{
+// 		*lst = (*lst)->next;
+// 	}
+// 	(*lst)->next = new;
+// }
+
+// void	ft_lstclear(t_list **lst, void (*del)(void *))
+// {
+// 	t_list	*next;
+// 	t_list	*current;
+
+// 	if (lst == NULL || del == NULL)
+// 		return ;
+// 	current = *lst;
+// 	while (current != NULL)
+// 	{
+// 		next = current->next;
+// 		del(current->content);
+// 		free(current);
+// 		current = next;
+// 	}
+// 	*lst = NULL;
+// }
+
+// int	ft_lstsize(t_list *lst)
+// {
+// 	int	size;
+
+// 	size = 0;
+// 	while (lst != NULL)
+// 	{
+// 		size++;
+// 		lst = lst->next;
+// 	}
+// 	return (size);
+// }
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	i;
+	size_t	s1_size;
+	size_t	s2_size;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	s1_size = ft_strlen(s1);
+	s2_size = ft_strlen(s2);
+	result = (char *)malloc((s1_size + s2_size + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s1_size || i <= s2_size)
+	{
+		if (i < s1_size)
+			result[i] = s1[i];
+		if (i <= s2_size)
+		result[s1_size + s2_size - i] = s2[s2_size - i];
+		i++;
+	}
+	return (result);
+}
